a6495cbd7d5f25ecb5f24d884b6732adabef9876
Anton <dev@atjn.dk>
2023-09-18 14:03:10 +0200

    Add an icon library

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
+<script lang="ts">
+	import EmoticonCoolOutline from "svelte-material-icons/EmoticonCoolOutline.svelte";
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<p>Example of using svelte material icons: <EmoticonCoolOutline /> all available icons can be found at <a href="https://pictogrammers.com/library/mdi/">https://pictogrammers.com/library/mdi/</a></p>
