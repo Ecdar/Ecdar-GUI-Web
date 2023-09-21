6bc576079b0d3e856f4ed87bfac6f46c56bca86f
BaBrixx <bastian.b.hansen@gmail.com>
2023-09-21 14:56:28 +0200

    Changed the Materials library

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
-	import EmoticonCoolOutline from "svelte-material-icons/EmoticonCoolOutline.svelte";
+	import { Hiking } from 'svelte-google-materialdesign-icons';
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
-<p>Example of using svelte material icons: <EmoticonCoolOutline /> all available icons can be found at <a href="https://pictogrammers.com/library/mdi/">https://pictogrammers.com/library/mdi/</a></p>
+<p>
+	Example of using svelte material icons: <Hiking /> all available icons can be found at
+	<a href="https://pictogrammers.com/library/mdi/">https://pictogrammers.com/library/mdi/</a>
+</p>
