7f25dd6a5ffbe230c9644bf79a2937a57bd25385
TSKsmiley <arnarfreyr29@gmail.com>
2023-09-28 11:09:52 +0200

    cahnge

---
+++ src/lib/components/svg-view/Location.svelte
@@ -0,0 +1,16 @@
+<svelte:options accessors/>
+
+<script lang="ts">
+
+export let x: number;
+	export let y: number;
+	export let locationID: string;
+
+	$: x;
+	$: y;
+	$: locationID;
+
+	import Node from "./Node.svelte";
+</script>
+
+<Node locationID={locationID} radius={10} x={x} y={y}/>
