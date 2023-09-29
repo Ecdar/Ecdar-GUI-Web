dbafeb8df1ba76a57d4a98e468949d1afa930090
Skrome <anders.nyby01@gmail.com>
2023-09-29 11:55:36 +0200

    More time for test!

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,7 +3,8 @@ import type { PlaywrightTestConfig } from '@playwright/test';
 const config: PlaywrightTestConfig = {
 	webServer: {
 		command: 'yarn build && yarn preview',
-		port: 4173
+		port: 4173,
+		timeout: 10 * 60 * 1000
 	},
 	testDir: 'tests',
 	testMatch: /(.+\.)?(test|spec)\.[jt]s/
