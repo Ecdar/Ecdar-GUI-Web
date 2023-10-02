0be40c4050b9aa2c0fc6a50541661a020e289bb8
Anton <dev@atjn.dk>
2023-10-02 15:46:56 +0200

    Fixed the comments
    Intellisense can now figure out what they are related to :)

--- src/lib/components/svelteModifiers/rightClickableComponents/ContextMenuItem.ts
+++ src/lib/components/svelteModifiers/rightClickableComponents/ContextMenuItem.ts
@@ -4,17 +4,25 @@ import type { ComponentType } from "svelte";
  * Class used for storing information about context menu items (The menu appearing when rightclicking)
  */
 export class ContextMenuItem {
-	/**	Property explanation:
-	 * isHorizontalRule: Used to determine whether the item should represent a splitter line or a clickable button
-	 * displayText: The text displayed on the clickable button
-	 * cssClass: Specify special style for different types of buttons
-	 * icon: The icon to be displayed before the displayText
-	 * onClick: The function that should be run whenever the button is clicked. Has reference to the right clicked element
+	/**
+	 * Used to determine whether the item should represent a splitter line or a clickable button.
 	 */
 	public isHorizontalRule: boolean;
+	/**
+	 * The text displayed on the clickable button.
+	 */
 	public displayText: string;
+	/**
+	 * The function that should be run whenever the button is clicked. Has reference to the right clicked element.
+	 */
 	public onClick: (originalRightClickable: HTMLElement) => void;
+	/**
+	 * The icon to be displayed before the displayText.
+	 */
 	public icon: ComponentType | undefined;
+	/**
+	 * Specify special style for different types of buttons.
+	 */
 	public cssClass: string | undefined;
 
 	constructor(
