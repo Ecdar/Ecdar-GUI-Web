bbb4fe6d5b717f4670fe1be348d8485c4678b45f
tr0ub1eM4k3r <61628220+tr0ub1eM4k3r@users.noreply.github.com>
2023-10-06 14:35:09 +0200

    Fixed issue where you could select the arrow

--- src/lib/components/console/ConsoleLine.svelte
+++ src/lib/components/console/ConsoleLine.svelte
@@ -3,12 +3,24 @@
 </script>
 
 <div>
-	<p>> {componentText}</p>
+	<nobr>
+		<p class="unselectable">></p>
+		<p>{componentText}</p>
+	</nobr>
 </div>
 
 <style>
 	p {
-		margin: 0.2em 1em 0.2em;
+		display: inline-block;
+		margin: 0.2em 0em 0.2em 0.5em;
 		text-shadow: 0 1.5px 1px rgba(28, 28, 28, 0.3);
+	}
+
+	.unselectable {
+		-moz-user-select: -moz-none;
+   		-khtml-user-select: none;
+   		-webkit-user-select: none;
+   		-ms-user-select: none;
+   		user-select: none;
 	}
 </style>
