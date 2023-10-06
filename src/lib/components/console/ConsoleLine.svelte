2ee2f9eb2de5500a9f7fa48c1a9a1a9e8fbc28c3
tr0ub1eM4k3r <61628220+tr0ub1eM4k3r@users.noreply.github.com>
2023-10-06 14:36:31 +0200

    Formatting

--- src/lib/components/console/ConsoleLine.svelte
+++ src/lib/components/console/ConsoleLine.svelte
@@ -18,9 +18,9 @@
 
 	.unselectable {
 		-moz-user-select: -moz-none;
-   		-khtml-user-select: none;
-   		-webkit-user-select: none;
-   		-ms-user-select: none;
-   		user-select: none;
+		-khtml-user-select: none;
+		-webkit-user-select: none;
+		-ms-user-select: none;
+		user-select: none;
 	}
 </style>
