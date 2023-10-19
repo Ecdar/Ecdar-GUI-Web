8804e76d9189277198b08d13b0e8683c859379aa
Skrome <anders.nyby01@gmail.com>
2023-10-19 13:48:06 +0200

    Toolbox? Questionmark?

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,19 @@
 	import LocationsWithContextMenu from "$lib/components/samplesImplementations/LocationsWithContextMenu.svelte";
 	import Queries from "$lib/components/query/Queries.svelte";
 	import QueryNav from "$lib/components/query/QueryNav.svelte";
+	import {
+		Person_pin_circle,
+		Vpn_key_off,
+		Screen_lock_landscape,
+		Table_restaurant,
+		Clean_hands,
+		Pregnant_woman,
+		Fire_extinguisher,
+	} from "svelte-google-materialdesign-icons";
+
+	const iconConfig = {
+		size: "40",
+	};
 
 	enum SidePanel {
 		Left,
@@ -63,8 +76,33 @@
 <main bind:this={mainContainer}>
 	<!-- Left side Panel -->
 	<div class="sidePanel" style="flex-basis: {leftSidePanelWidth}px">
-		<nav class="inner-nav1">Nav 1</nav>
-		<p>Left</p>
+		<nav class="inner-nav1">Toolbox</nav>
+		<p>Tool</p>
+		<div class="icons">
+			<div class="icon">
+				<Person_pin_circle {...iconConfig} />
+			</div>
+			<div class="icon">
+				<Vpn_key_off {...iconConfig} />
+			</div>
+			<div class="icon">
+				<Screen_lock_landscape {...iconConfig} />
+			</div>
+			<div class="icon">
+				<Table_restaurant {...iconConfig} />
+			</div>
+			<div class="icon">
+				<Clean_hands {...iconConfig} />
+			</div>
+			<div class="icon">
+				<Pregnant_woman {...iconConfig} />
+			</div>
+			<div class="icon">
+				<Fire_extinguisher {...iconConfig} />
+			</div>
+		</div>
+		<nav class="inner-nav1">Project</nav>
+		<p>Project</p>
 	</div>
 	<!-- Left resize Panel -->
 	<div
@@ -142,6 +180,20 @@
 	.canvas {
 		background-color: whitesmoke;
 		flex-grow: 1;
+	}
+
+	.icons {
+		display: flex;
+		flex-wrap: wrap;
+	}
+
+	.icon {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		cursor: pointer;
+		border: 2px solid black;
+		margin: 2px;
 	}
 
 	footer {
