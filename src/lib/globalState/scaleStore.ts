70b8c4466342d334d9f6f3578145876951547cce
Anton <dev@atjn.dk>
2023-10-20 22:40:43 +0200

    Fix typo

--- src/lib/globalState/scaleStore.ts
+++ src/lib/globalState/scaleStore.ts
@@ -1,7 +1,7 @@
 import { writable, type Writable } from "svelte/store";
 
 /**
- * Dines the current scaling of the main SVG view.
+ * Defines the current scaling of the main SVG view.
  * We need to know the current scaling when calculating the position of locations based on the mouse position.
  */
 export const scale: Writable<number> = writable(1);
