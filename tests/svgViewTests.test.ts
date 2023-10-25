24716a72a0e199e17741b64c351be8ae7a0e342d
Anton <dev@atjn.dk>
2023-10-25 14:19:53 +0200

    Format

--- tests/svgViewTests.test.ts
+++ tests/svgViewTests.test.ts
@@ -1,57 +1,55 @@
-import {expect, test} from "@playwright/test";
+import { expect, test } from "@playwright/test";
 
-test('drag and drop a location to a new position', async ({page}) => {
-	await page.goto('/');
+test("drag and drop a location to a new position", async ({ page }) => {
+	await page.goto("/");
 
-	const svg = page.locator('#node1');
+	const svg = page.locator("#node1");
 
 	// Get the origiganl position of the element
-	const svgY = parseInt(await svg.getAttribute('cy') ?? "1");
-	const svgX = parseInt(await svg.getAttribute('cx') ?? "1");
+	const svgY = parseInt((await svg.getAttribute("cy")) ?? "1");
+	const svgX = parseInt((await svg.getAttribute("cx")) ?? "1");
 
-/* 	Expected value of the element (there is an offset of 20 because the 
-*	element is dragged from its edge in the padding of the svg)
-*/
+	/* 	Expected value of the element (there is an offset of 20 because the
+	 *	element is dragged from its edge in the padding of the svg)
+	 */
 	const expectedX = svgX;
 	const expectedY = svgY + 80;
-	
+
 	console.log(svgX, svgY);
 	//Offset by 80
-	await svg.dragTo(svg, {targetPosition: {x: 20, y: 100}, force: true});
-
+	await svg.dragTo(svg, { targetPosition: { x: 20, y: 100 }, force: true });
 
 	// New values x and y position of the element
-	const resultX = parseInt(await svg.getAttribute('cx') ?? "1");
-	const resultY = parseInt(await svg.getAttribute('cy') ?? "1");
+	const resultX = parseInt((await svg.getAttribute("cx")) ?? "1");
+	const resultY = parseInt((await svg.getAttribute("cy")) ?? "1");
 
 	// Check if the element has moved
 	expect(resultX).toBe(expectedX);
 	expect(resultY).toBe(expectedY);
 });
 
-test('drag and drop a nail', async ({page}) => {
-	await page.goto('/');
+test("drag and drop a nail", async ({ page }) => {
+	await page.goto("/");
 
-	const svg = page.locator('#node\\!');
+	const svg = page.locator("#node\\!");
 
 	// Get the origiganl position of the element
-	const svgY = parseInt(await svg.getAttribute('cy') ?? "1");
-	const svgX = parseInt(await svg.getAttribute('cx') ?? "1");
+	const svgY = parseInt((await svg.getAttribute("cy")) ?? "1");
+	const svgX = parseInt((await svg.getAttribute("cx")) ?? "1");
 
-/* 	Expected value of the element (there is an offset of 10 because the 
-*	element is dragged from its edge in the padding of the svg)
-*/
+	/* 	Expected value of the element (there is an offset of 10 because the
+	 *	element is dragged from its edge in the padding of the svg)
+	 */
 	const expectedX = svgX;
 	const expectedY = svgY + 80;
-	
+
 	console.log(svgX, svgY);
 	//Offset by 80
-	await svg.dragTo(svg, {targetPosition: {x: 10, y: 90}, force: true});
-
+	await svg.dragTo(svg, { targetPosition: { x: 10, y: 90 }, force: true });
 
 	// New values x and y position of the element
-	const resultX = parseInt(await svg.getAttribute('cx') ?? "1");
-	const resultY = parseInt(await svg.getAttribute('cy') ?? "1");
+	const resultX = parseInt((await svg.getAttribute("cx")) ?? "1");
+	const resultY = parseInt((await svg.getAttribute("cy")) ?? "1");
 
 	// Check if the element has moved
 	expect(resultX).toBe(expectedX);
