994d11cd235b1f9635b925e24a5d54a67fb703a4
TSKsmiley <arnarfreyr29@gmail.com>
2023-10-26 15:48:36 +0200

    format

--- src/lib/components/svg-view/state.ts
+++ src/lib/components/svg-view/state.ts
@@ -9,16 +9,15 @@ export const locationRecord: Writable<Record<string, Location>> = writable({});
 
 // update record
 activeView.subscribe((view) => {
-    if (view === undefined) return;
-    if (view instanceof Component) {
-        for (const location of view.locations) {
-            locationRecord.update(currentRecord => {
-                
-                // Make a copy of the record, set the new value, and return it
-                const updatedRecord = {...currentRecord}; // Using object spread to create a shallow copy
-                updatedRecord[location.id] = location;
-                return updatedRecord;
-            });
-        }
-    }
+	if (view === undefined) return;
+	if (view instanceof Component) {
+		for (const location of view.locations) {
+			locationRecord.update((currentRecord) => {
+				// Make a copy of the record, set the new value, and return it
+				const updatedRecord = { ...currentRecord }; // Using object spread to create a shallow copy
+				updatedRecord[location.id] = location;
+				return updatedRecord;
+			});
+		}
+	}
 });
