44026ce27ea2e24a452365192d37c026ce3cb5c8
DenFlyvendeGed <simont1998@gmail.com>
2023-10-26 11:09:19 +0200

    class and fields defined

---
+++ src/lib/classes/engine/Engine.ts
@@ -0,0 +1,49 @@
+import type {EngineType} from "./EngineTypes"
+
+/**
+ * # A Reveaal, JEcdar og API engine definition
+ * It stores the IP and port of an engine
+ * */
+export class Engine
+{
+	/**
+	 * The name of the Engine
+	 * */
+	name: string = "";
+
+	/**
+	 * The IP address of the engine
+	 * */
+	address: string = "";
+
+	/**
+	 * The starting number of the portrange
+	 * */
+	portRangeStart: number = 0;
+
+	/**
+	 * The last number of the portrange
+	 * */
+	portRangeEnd: number = 0;
+
+	/**
+	 * Type of engine
+	 * */
+	type: EngineType; //enum 
+
+	
+
+	constructor(
+		name: string,
+		address: string,
+	    portRangeStart: number,
+	    portRangeEnd: number,
+	    type: EngineType, //enum 
+	) {
+		this.name = name;
+		this.address = address;
+		this.portRangeStart = portRangeStart;
+		this.portRangeEnd = portRangeEnd;
+		this.type = type;
+	}
+}
---
+++ src/lib/classes/engine/EngineTypes.ts
@@ -0,0 +1,8 @@
+/**
+ * Emum for choosing what engine type is communicated with
+ */
+export const enum EngineType {
+	Reveaal,
+	JEcdar,
+	API,
+}
