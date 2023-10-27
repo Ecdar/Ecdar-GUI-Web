9fb35d1f3d4e970820d0b55a201a6f29e0b5e71d
TSKsmiley <arnarfreyr29@gmail.com>
2023-10-27 12:49:46 +0200

    example

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,17 +1,58 @@
 <script lang="ts">
-	import { Component, Location, Nickname } from "$lib/classes/automaton";
+	import {
+		Component,
+		Location,
+		Nickname,
+		Edge,
+		Status,
+		Nail,
+		PropertyType,
+		Property,
+	} from "$lib/classes/automaton";
 	import { Point } from "$lib/classes/draw";
 	import { Project } from "$lib/classes/project/Project";
 	import ContextMenu from "$lib/components/contextMenu/ContextMenu.svelte";
 	import { activeView, project } from "$lib/globalState/activeProject";
 
-	const comp = new Component("bing", "", [
-		new Location(
-			"1",
-			new Point(100, 100),
-			new Nickname("testing", new Point(40, -40)),
-		),
-	]);
+	const comp = new Component(
+		"bing",
+		"",
+		[
+			new Location(
+				"1",
+				new Point(100, 100),
+				new Nickname("testing", new Point(40, -40)),
+			),
+			new Location(
+				"2",
+				new Point(200, 200),
+				new Nickname("testing2", new Point(40, -40)),
+			),
+		],
+		[
+			new Edge(
+				"test",
+				undefined,
+				"1",
+				"2",
+				Status.OUTPUT,
+				undefined,
+				undefined,
+				undefined,
+				undefined,
+				undefined,
+				[
+					new Nail(
+						new Property(
+							PropertyType.SYNCHRONIZATION,
+							new Point(150, 150),
+						),
+						new Point(150, 150),
+					),
+				],
+			),
+		],
+	);
 
 	project.set(new Project("Test project", [comp]));
 
