6aa3d5a9e91027c8ceec290abbf54d3994c29901
tr0ub1eM4k3r <61628220+tr0ub1eM4k3r@users.noreply.github.com>
2023-10-30 08:39:07 +0100

    Made general Dialog component
    Co-authored-by: SolarEarth37 <SolarEarth37@users.noreply.github.com>

---
+++ src/lib/components/dialogPopover/Dialog.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	let dialogContainer: HTMLDialogElement;
+
+	/**
+	 * Function for showing the current modal
+	 */
+	export function showModal() {
+		dialogContainer?.showModal();
+	}
+</script>
+
+<dialog bind:this={dialogContainer}>
+	<form method="dialog">
+		<div class="box">
+			<slot />
+		</div>
+	</form>
+</dialog>
+
+<style>
+	.box {
+		max-width: 100vw;
+		display: block;
+		position: fixed;
+		background-color: lightslategray;
+		z-index: 100;
+		transform: translate(-50%, -50%);
+		top: 50%;
+		left: 50%;
+		padding: 1rem;
+		backdrop-filter: brightness(0.2);
+	}
+</style>
---
+++ src/lib/components/engineUI/EngineUI.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import Dialog from "../dialogPopover/Dialog.svelte";
+
+	let formElement: HTMLFormElement;
+	let dialogContainer: Dialog;
+
+	export function showEngineUI() {
+		dialogContainer.showModal();
+	}
+</script>
+
+<button on:click={showEngineUI}> Show</button>
+<Dialog bind:this={dialogContainer}>
+	<form bind:this={formElement}>
+		<label for="IP">IP Address:</label>
+		<input type="text" placeholder="IP" name="IP" />
+		<button type="button">Confirm</button>
+	</form>
+
+	<button>Close</button>
+</Dialog>
+
+<style>
+</style>
