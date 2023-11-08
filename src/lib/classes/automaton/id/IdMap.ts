d2ec3683774a22d9a77b46e90b3767a2814500a8
Anton <dev@atjn.dk>
2023-11-08 14:37:15 +0100

    Format

--- src/lib/classes/automaton/id/IdMap.ts
+++ src/lib/classes/automaton/id/IdMap.ts
@@ -2,21 +2,27 @@ import { AutomatonClass } from "../AutomatonClass";
 import type { HasId } from "./HasId";
 import { Id } from "./Id";
 
-export abstract class IdSet<T extends number | string, C extends HasId<I>, I extends Id<T>, R>
-	extends AutomatonClass<R>
-{
-	constructor(
-		private idConstructor: new (rawId: number | string) => I, 
-	){super()}
+export abstract class IdSet<
+	T extends number | string,
+	C extends HasId<I>,
+	I extends Id<T>,
+	R,
+> extends AutomatonClass<R> {
+	constructor(private idConstructor: new (rawId: number | string) => I) {
+		super();
+	}
 
 	private idMap = new Map<T extends string ? string : number | string, I>();
 
 	private orderedMap: (C | undefined)[] = [];
-	private unorderedMap = new Map<T extends string ? string : number | string, C>();
+	private unorderedMap = new Map<
+		T extends string ? string : number | string,
+		C
+	>();
 
 	private nextOrderedIndex: number = 0;
-	private findNextOrderedIndex(){
-		while(this.orderedMap[this.nextOrderedIndex] !== undefined){
+	private findNextOrderedIndex() {
+		while (this.orderedMap[this.nextOrderedIndex] !== undefined) {
 			this.nextOrderedIndex++;
 		}
 	}
@@ -26,53 +32,63 @@ export abstract class IdSet<T extends number | string, C extends HasId<I>, I ext
 		return this.#size;
 	}
 
-	getNewId(rawId: T extends string ? string : number | string | undefined): I | undefined {
-		if(!rawId){
+	getNewId(
+		rawId: T extends string ? string : number | string | undefined,
+	): I | undefined {
+		if (!rawId) {
 			this.findNextOrderedIndex();
 			rawId = this.nextOrderedIndex;
-		}else if(this.hasId(rawId)) {
-			throw new TypeError()
+		} else if (this.hasId(rawId)) {
+			throw new TypeError();
 		}
-			
+
 		const newId = new this.idConstructor(rawId);
 		this.setId(newId);
 
 		return newId;
 	}
 
-	private setId(id: I){
+	private setId(id: I) {
 		return this.idMap.set(id.toRaw(), id);
 	}
 
 	getId = this.idMap.get;
-	
+
 	hasId = this.idMap.has;
 
 	add(member: C) {
 		const idOrder = member.id.order;
 		if (idOrder !== undefined) {
 			if (this.orderedMap[idOrder] !== undefined) {
-				throw new TypeError(`The member "${member.id.toRaw()}" cannot be added because it already exists`);
+				throw new TypeError(
+					`The member "${member.id.toRaw()}" cannot be added because it already exists`,
+				);
 			}
 		} else {
 			const rawId = member.id.toRaw();
 			if (this.unorderedMap.has(rawId) === true) {
-				throw new TypeError(`The member "${member.id.toRaw()}" cannot be added because it already exists`);
+				throw new TypeError(
+					`The member "${member.id.toRaw()}" cannot be added because it already exists`,
+				);
 			}
 		}
 		return this.set(member);
 	}
 
-	update(member: C){
+	update(member: C) {
 		const idOrder = member.id.order;
 		if (idOrder !== undefined) {
 			if (this.orderedMap[idOrder] === undefined) {
-				throw new TypeError(`The member "${member.id.toRaw()}" cannot be updated because it does not exist`);
+				throw new TypeError(
+					`The member "${member.id.toRaw()}" cannot be updated because it does not exist`,
+				);
 			}
 		} else {
 			const rawId = member.id.toRaw();
 			if (this.unorderedMap.has(rawId) === false) {
-				throw new TypeError(`The member "${member.id.toRaw()}" cannot be updated because it does not exist`);
+				throw new TypeError(
+					`The member "${member.id.toRaw()}" cannot be updated because it does not exist`,
+				);
 			}
 		}
 		return this.set(member);
@@ -110,7 +126,7 @@ export abstract class IdSet<T extends number | string, C extends HasId<I>, I ext
 			if (this.orderedMap[idOrder] !== undefined) {
 				this.orderedMap[idOrder] === undefined;
 				this.#size--;
-				if(idOrder < this.nextOrderedIndex){
+				if (idOrder < this.nextOrderedIndex) {
 					this.nextOrderedIndex = idOrder;
 				}
 				return true;
