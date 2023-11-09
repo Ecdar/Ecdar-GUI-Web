b2ea2da7b734fef897dfea75db7adc1ba7578036
TSKsmiley <arnarfreyr29@gmail.com>
2023-11-09 13:53:12 +0100

    Fix tests and add new ones
    Co-authored-by: Nyby <NybyDK@users.noreply.github.com>

--- tests/project.test.ts
+++ tests/project.test.ts
@@ -86,7 +86,7 @@ test("Delete 10 systems", async ({ page }) => {
 	}
 
 	await expect(page.locator(".project-item.system")).toHaveCount(10);
-
+	
 	for (let i = 0; i < 10; i++) {
 		await page.click("#system-button-0");
 		await page
@@ -98,6 +98,7 @@ test("Delete 10 systems", async ({ page }) => {
 	await expect(page.locator(".project-item.system")).toHaveCount(0);
 });
 
+
 test("Delete 10 components", async ({ page }) => {
 	await expect(page.locator(".project-item.component")).toHaveCount(0);
 
@@ -106,7 +107,7 @@ test("Delete 10 components", async ({ page }) => {
 	}
 
 	await expect(page.locator(".project-item.component")).toHaveCount(10);
-
+	
 	for (let i = 0; i < 10; i++) {
 		await page.click("#component-button-0");
 		await page
@@ -114,6 +115,72 @@ test("Delete 10 components", async ({ page }) => {
 			.getByRole("button", { name: "Delete" })
 			.click();
 	}
-
+	
 	await expect(page.locator(".project-item.component")).toHaveCount(0);
+});
+
+test("Delete the grey component", async ({ page }) => {
+	for (let i = 0; i < 3; i++) {
+		await page.click("#add-component");
+	
+		await page.click(`#component-button-${i}`);
+		await page
+			.locator(`#component-menu-${i}`).locator(`.colors > button:nth-child(${i + 1})`).first().click();
+	}
+	
+	await page.click("#component-button-0");
+	await page
+		.locator("#component-menu-0")
+		.getByRole("button", { name: "Delete" })
+		.click();
+
+	const components = page.locator(".project-item.component");
+
+	await expect(components).toHaveCount(2);
+	await expect(components.nth(0).locator(".circle")).toHaveCSS("background-color", "rgb(255, 165, 0)");
+	await expect(components.nth(1).locator(".circle")).toHaveCSS("background-color", "rgb(255, 0, 0)");
+})
+
+
+test("Can toggle includeInPeriodicCheck", async ({ page }) => {
+	await page.click("#add-component");
+
+	const svgPath = page
+		.locator("#component-menu-0")
+		.locator("svg")
+		.first()
+		.locator("path");
+
+	const firstPath = await svgPath.getAttribute("d");
+
+	await page.click("#component-button-0");
+	await page
+		.locator("#component-menu-0")
+		.getByRole("button", { name: "Include in periodic check" })
+		.click();
+
+	const secondPath = await svgPath.getAttribute("d");
+
+	expect(firstPath).not.toBe(secondPath);
+
+	await page.click("#component-button-0");
+	await page
+		.locator("#component-menu-0")
+		.getByRole("button", { name: "Include in periodic check" })
+		.click();
+
+	const thirdPath = await svgPath.getAttribute("d");
+
+	expect(firstPath).toBe(thirdPath);
+});
+
+test("Description should stay the same after edited", async ({ page }) => {
+	await page.click("#add-component");
+
+	await page.click("#component-button-0")
+	await page.locator("#component-menu-0").getByRole("textbox").fill("This is a new description.");
+	await page.keyboard.press("Escape");
+
+	await page.click("#component-button-0")
+	await expect(page.locator("#component-menu-0").getByRole("textbox")).toHaveValue("This is a new description.");
 });
