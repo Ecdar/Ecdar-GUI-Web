df1616eb165cfa5fcde4c4e30c5081fec1badff9
Arnar Freyr <Arnarfreyr29@gmail.com>
2023-11-10 13:46:06 +0100

    69 create systems comps (#102)
    * Add systems and components
    
    * Maybe this way to rename?
    
    * Moved handler so can click if there is no text
    
    For example if user names it whitespace it can be hard to rename
    
    * Sveltify and add description
    
    * Color options
    
    Systems and Components files are almost identical
    
    * Type
    
    * Merge components and systems into 1 file
    
    * Drop down menu for project items
    
    * Delete ComponentDropDownMenu.svelte
    
    * Update ProjectItemDropDownMenu.svelte
    
    * Fix periodic check binding in ProjectItems and add
    dynamic class to ProjectItem
    
    * 40 Implemented dynamic global css variables (#89)
    
    * Implemented everything
    
    I made a huge mistake and merge comitted the branch linked to issue #50 into my original CSS branch. This included every individual commits from the other branch into my CSS branch. I tried to revert it but failed and was therefore forced to use this solution.
    
    * Added dark mode color to edges
    
    * Created a CSS variable support check
    
    This makes sure that no unsupported CSS variables are used. The supported CSS variables are specified  in CSSVariables.ts
    
    * Changed resizer size
    
    * Removed unnecessary media schemes
    
    * Removed mediaFeature check
    
    * Corrrect the parsed error type
    
    * Removed the passing of the global window variable
    
    * Removed the need for range checking by using zod
    
    * Improved the zod schemes
    
    The zod schemes now enforce:
    - The color value range
    - That the "default" mediascheme contains a specification of all specified CSS variables
    
    and more...
    
    * Fix typo
    
    * Did some color styling to the navbar
    
    * Add IDs to add component and add system buttons in
    ProjectNav component
    
    * css variables
    
    * Update project item and dropdown menu components
    Co-authored-by: Nyby <NybyDK@users.noreply.github.com>
    
    * Remove unused imports and clean up code.
    
    * Fix tests and add new ones
    
    Co-authored-by: Nyby <NybyDK@users.noreply.github.com>
    
    * lint
    
    ---------
    
    Co-authored-by: Skrome <anders.nyby01@gmail.com>
    Co-authored-by: Nyby <nyby.dev@gmail.com>
    Co-authored-by: Bastian Brix <90306530+BaBrixx@users.noreply.github.com>
    Co-authored-by: Nyby <NybyDK@users.noreply.github.com>

---
+++ tests/project.test.ts
@@ -0,0 +1,198 @@
+import { test, expect } from "@playwright/test";
+
+test.beforeEach(async ({ page }) => {
+	await page.goto("/");
+	await page.click("#start-new-project");
+});
+
+test("Starts only with Global Declaration", async ({ page }) => {
+	await expect(page.locator(".global-dec")).toHaveCount(1);
+	await expect(page.locator(".project-item")).toHaveCount(0);
+});
+
+test("Add a component", async ({ page }) => {
+	await expect(page.locator(".project-item.component")).toHaveCount(0);
+	await page.click("#add-component");
+	await expect(page.locator(".project-item.component")).toHaveCount(1);
+});
+
+test("Add a system", async ({ page }) => {
+	await expect(page.locator(".project-item.system")).toHaveCount(0);
+	await page.click("#add-system");
+	await expect(page.locator(".project-item.system")).toHaveCount(1);
+});
+
+test("Add 10 components", async ({ page }) => {
+	await expect(page.locator(".project-item.component")).toHaveCount(0);
+
+	for (let i = 0; i < 10; i++) {
+		await page.click("#add-component");
+	}
+
+	await expect(page.locator(".project-item.component")).toHaveCount(10);
+});
+
+test("Add 10 systems", async ({ page }) => {
+	await expect(page.locator(".project-item.system")).toHaveCount(0);
+
+	for (let i = 0; i < 10; i++) {
+		await page.click("#add-system");
+	}
+
+	await expect(page.locator(".project-item.system")).toHaveCount(10);
+});
+
+test("Add 5 systems and 5 components", async ({ page }) => {
+	await expect(page.locator(".project-item")).toHaveCount(0);
+
+	for (let i = 0; i < 5; i++) {
+		await page.click("#add-component");
+		await page.click("#add-system");
+	}
+
+	await expect(page.locator(".project-item.component")).toHaveCount(5);
+	await expect(page.locator(".project-item.system")).toHaveCount(5);
+});
+
+test("Delete a component", async ({ page }) => {
+	await expect(page.locator(".project-item.component")).toHaveCount(0);
+	await page.click("#add-component");
+	await expect(page.locator(".project-item.component")).toHaveCount(1);
+	await page.click("#component-button-0");
+	await page
+		.locator("#component-menu-0")
+		.getByRole("button", { name: "Delete" })
+		.click();
+	await expect(page.locator(".project-item.component")).toHaveCount(0);
+});
+
+test("Delete a system", async ({ page }) => {
+	await expect(page.locator(".project-item.system")).toHaveCount(0);
+	await page.click("#add-system");
+	await expect(page.locator(".project-item.system")).toHaveCount(1);
+	await page.click("#system-button-0");
+	await page
+		.locator("#system-menu-0")
+		.getByRole("button", { name: "Delete" })
+		.click();
+	await expect(page.locator(".project-item.system")).toHaveCount(0);
+});
+
+test("Delete 10 systems", async ({ page }) => {
+	await expect(page.locator(".project-item.system")).toHaveCount(0);
+
+	for (let i = 0; i < 10; i++) {
+		await page.click("#add-system");
+	}
+
+	await expect(page.locator(".project-item.system")).toHaveCount(10);
+
+	for (let i = 0; i < 10; i++) {
+		await page.click("#system-button-0");
+		await page
+			.locator("#system-menu-0")
+			.getByRole("button", { name: "Delete" })
+			.click();
+	}
+
+	await expect(page.locator(".project-item.system")).toHaveCount(0);
+});
+
+test("Delete 10 components", async ({ page }) => {
+	await expect(page.locator(".project-item.component")).toHaveCount(0);
+
+	for (let i = 0; i < 10; i++) {
+		await page.click("#add-component");
+	}
+
+	await expect(page.locator(".project-item.component")).toHaveCount(10);
+
+	for (let i = 0; i < 10; i++) {
+		await page.click("#component-button-0");
+		await page
+			.locator("#component-menu-0")
+			.getByRole("button", { name: "Delete" })
+			.click();
+	}
+
+	await expect(page.locator(".project-item.component")).toHaveCount(0);
+});
+
+test("Delete the grey component", async ({ page }) => {
+	for (let i = 0; i < 3; i++) {
+		await page.click("#add-component");
+
+		await page.click(`#component-button-${i}`);
+		await page
+			.locator(`#component-menu-${i}`)
+			.locator(`.colors > button:nth-child(${i + 1})`)
+			.first()
+			.click();
+	}
+
+	await page.click("#component-button-0");
+	await page
+		.locator("#component-menu-0")
+		.getByRole("button", { name: "Delete" })
+		.click();
+
+	const components = page.locator(".project-item.component");
+
+	await expect(components).toHaveCount(2);
+	await expect(components.nth(0).locator(".circle")).toHaveCSS(
+		"background-color",
+		"rgb(255, 165, 0)",
+	);
+	await expect(components.nth(1).locator(".circle")).toHaveCSS(
+		"background-color",
+		"rgb(255, 0, 0)",
+	);
+});
+
+test("Can toggle includeInPeriodicCheck", async ({ page }) => {
+	await page.click("#add-component");
+
+	const svgPath = page
+		.locator("#component-menu-0")
+		.locator("svg")
+		.first()
+		.locator("path");
+
+	const firstPath = await svgPath.getAttribute("d");
+
+	await page.click("#component-button-0");
+	await page
+		.locator("#component-menu-0")
+		.getByRole("button", { name: "Include in periodic check" })
+		.click();
+
+	const secondPath = await svgPath.getAttribute("d");
+
+	expect(firstPath).not.toBe(secondPath);
+
+	await page.click("#component-button-0");
+	await page
+		.locator("#component-menu-0")
+		.getByRole("button", { name: "Include in periodic check" })
+		.click();
+
+	const thirdPath = await svgPath.getAttribute("d");
+
+	expect(firstPath).toBe(thirdPath);
+});
+
+test("Description should stay the same after edited", async ({ page }) => {
+	await page.click("#add-component");
+
+	await page.click("#component-button-0");
+	await page
+		.locator("#component-menu-0")
+		.getByRole("textbox")
+		.fill("This is a new description.");
+	await page.keyboard.press("Escape");
+
+	await page.click("#component-button-0");
+	await expect(
+		page.locator("#component-menu-0").getByRole("textbox"),
+	).toHaveValue("This is a new description.");
+});
