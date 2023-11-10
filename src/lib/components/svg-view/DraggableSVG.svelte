8553a82ec41cfd3347d9421bcf76be12b680b418
Anton <dev@atjn.dk>
2023-11-10 10:40:11 +0100

    Update active view correctly

--- src/lib/components/svg-view/DraggableSVG.svelte
+++ src/lib/components/svg-view/DraggableSVG.svelte
@@ -22,13 +22,16 @@
 		position.x += event.movementX / $scale;
 		position.y += event.movementY / $scale;
 
-		// Update the active model
+		// Update the active view partially
 		activeView.fastUpdate();
 	}
 
 	//Removes the eventlisteners when the mouse is released using abortcontroller
 	function onPointerUp() {
 		controller.abort();
+
+		// Update the active view properly
+		$activeView = $activeView;
 	}
 </script>
 
