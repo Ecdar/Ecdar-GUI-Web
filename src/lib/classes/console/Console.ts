d31ddf802660b0a76728101fcdc634a90b55bff2
DenFlyvendeGed <simont1998@gmail.com>
2023-11-21 14:18:56 +0100

    Added unit test

--- src/lib/classes/console/Console.ts
+++ src/lib/classes/console/Console.ts
@@ -1,16 +1,40 @@
 import { Tabs } from "$lib/classes/Tabs";
-import { writable, type Writable } from "svelte/store";
+import { writable, get, type Writable } from "svelte/store";
 
 class Console {
 	frontendConsoleLines: Writable<string[]> = writable([]);
 	backendConsoleLines: Writable<string[]> = writable([]);
 
+	/**
+	 *Function for sending an error to a specific tab in the console
+	 *@param textLine - The textline to be printed by the Console
+	 */
 	writeLineFrontend(textLine: string) {
 		this.frontendConsoleLines.update((items) => [...items, textLine]);
 	}
 
+	/**
+	 *Function for sending an error to a specific tab in the console
+	 *@param textLine - The textline to be printed by the Console
+	 */
 	writeLineBackend(textLine: string) {
 		this.backendConsoleLines.update((items) => [...items, textLine]);
+	}
+
+	/**
+	 *Function for getting a copy of the frontend tab's store
+	 *@returns Frontend tab's string array
+	 */
+	getFrontendArray(): string[]{
+		return get(this.frontendConsoleLines);
+	}
+
+	/**
+	 *Function for getting a copy of the backend tab's store
+	 *@returns Backend tab's string array
+	 */
+	getBackendArray(): string[]{
+		return get(this.backendConsoleLines);
 	}
 
 	/**
