1a9d0ab2923e5f346a1aa7760ea27637b52d301b
TSKsmiley <arnarfreyr29@gmail.com>
2023-11-23 16:06:13 +0100

    progress for today

--- src/lib/components/startScreen/StartScreen.svelte
+++ src/lib/components/startScreen/StartScreen.svelte
@@ -1,21 +1,37 @@
 <script lang="ts">
 	import { Project } from "$lib/classes/project/Project";
 	import { project } from "$lib/globalState/activeProject";
+	import SvgButton from "../buttons/SvgButton.svelte";
 
 	function startNewProject() {
 		$project = new Project();
 	}
+
+	function openProject() {
+		// Add your logic to open a project here
+	}
 </script>
 
-<div>
-	<h1>Welcome to Ecdar</h1>
-	<button id="start-new-project" on:click={startNewProject}
-		>Start a new project</button
+<div
+	style="display: flex; width: 100%; height: 100vh; justify-content: center;"
+>
+	<div
+		style="display: flex; flex-direction: column; width: 50%;  background-color: red;"
 	>
+		<h1>Welcome to Ecdar</h1>
+		<SvgButton
+			click={startNewProject}
+			icon={More_vert}
+			id={`button`}
+			color="var(--sidebar-text-color)"
+		/>
+	</div>
+	<div
+		style="display: flex; flex-direction: column; width: 50%; background-color: blue;"
+	>
+		<button id="open-project" on:click={openProject}>Open a project</button>
+	</div>
 </div>
 
 <style>
-	h1 {
-		color: var(--canvas-text-color);
-	}
 </style>
