0e7c0c203638b5611de469995c2e95974cd404b7
ChristianSkou <christian140398@gmail.com>
2023-11-23 14:59:08 +0100

    lint

--- tests/topbar.test.ts
+++ tests/topbar.test.ts
@@ -5,17 +5,51 @@ test.beforeEach(async ({ page }) => {
 	await page.click("#start-new-project");
 });
 
-test('Clicking about button', async ({ context, page }) => {
-	const pagePromise = context.waitForEvent('page');
+test("Check and uncheck Project Panel checkbox", async ({ page }) => {
+	await page.getByRole("button", { name: "View", exact: true }).click();
+	let color = await page
+		.getByRole("button", { name: "done Project Panel" })
+		.locator("svg")
+		.getAttribute("fill");
+	expect(color).toBe("transparent");
 
-	await page.getByRole('button', { name: 'Help', exact: true }).click();
-	page.on("dialog", async dialog => {
-		await dialog.accept();
-	})
-	await page.getByRole('button', { name: 'error About' }).click();
+	await page.getByRole("button", { name: "done Project Panel" }).click();
+	color = await page
+		.getByRole("button", { name: "done Project Panel" })
+		.locator("svg")
+		.getAttribute("fill");
+	expect(color).toBe("black");
+
+	await page.getByRole("button", { name: "done Project Panel" }).click();
+	color = await page
+		.getByRole("button", { name: "done Project Panel" })
+		.locator("svg")
+		.getAttribute("fill");
+	expect(color).toBe("transparent");
+});
+
+test("Clicking about button", async ({ context, page }) => {
+	const pagePromise = context.waitForEvent("page");
+
+	await page.getByRole("button", { name: "Help", exact: true }).click();
+
+	const popup = new Promise((res, rej) => {
+		page.on("dialog", (dialog) => {
+			dialog
+				.accept()
+				.then(() => {
+					res(undefined);
+				})
+				.catch(() => {
+					rej();
+				});
+		});
+	});
+
+	await page.getByRole("button", { name: "error About" }).click();
 
 	const newPage = await pagePromise;
 	await newPage.waitForLoadState();
-	await expect(await newPage.title()).toEqual("ECDAR");
-
-  });
+	await popup;
+	expect(await newPage.title()).toEqual("ECDAR");
+});
