af49b74c9e1b95658f774f84597c9d3c6fe6f099
KamyaPA <peteraksel@live.dk>
2023-11-23 10:09:43 +0100

    redone compile-protobuffers.js

--- compile-protobuffers.js
+++ compile-protobuffers.js
@@ -1,4 +1,4 @@
-import { execSync } from "child_process";
+import { exec } from "child_process";
 import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
@@ -8,17 +8,39 @@ let out_dir = `${dir}/src/lib/proto`;
 
 if (!fs.existsSync(out_dir)) fs.mkdirSync(out_dir);
 
-fs.readdir(`${dir}/Ecdar-ProtoBuf`, (err, files) => {
-	if (err !== null) throw err;
-	for (let file of files.filter((name) => name.match(/.*\.proto/g))) {
-		const cmd = `npx protoc --ts_out ${out_dir} --proto_path ${dir}/Ecdar-ProtoBuf ${dir}/Ecdar-ProtoBuf/${file}`;
-		execSync(cmd, (err, stdout, stderr) => {
-			console.log(`Executing commmand: "${cmd}"`);
+async function main() {
+	let files = await readdir(`${dir}/Ecdar-ProtoBuf`);
+	await Promise.all(files.filter(name => name.match(/.*\.proto/g)).map(file => 
+		runCmd(`
+		  npx protoc \\
+			  --ts_out ${out_dir} \\
+			  --proto_path ${dir}/Ecdar-ProtoBuf \\
+			  ${dir}/Ecdar-ProtoBuf/${file}
+		  `)
+	));
+	console.log("Finnished compiling protobuffers");
+}
+
+function readdir(dir){
+	return new Promise((res, rej) => {
+		fs.readdir(dir, (err, files) => {
+			if (err !== null) rej(err);
+			else res(files);
+		});
+	})
+}
+
+function runCmd(cmd){
+	return new Promise((res, _) => {
+		console.log(`Executing commmand: "${cmd}"`);
+		exec(cmd, (err, stdout, stderr) => {
 			if (stdout !== null) console.log(stdout);
 			if (stderr !== null) console.error(stderr);
 			if (err !== null) console.error(err);
+			res();
 		});
-	}
+	})
+}
 
-	console.log("Finnished compiling protobuffers");
-});
+main();
+
