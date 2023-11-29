e115e491801c70145c181204b8229ef45537bbf4
KamyaPA <peteraksel@live.dk>
2023-11-29 12:38:53 +0100

    compileProtobuffers as vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vitest/config";
+import { compileProtobuffers } from "./compileProtobuffers";
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), compileProtobuffers],
 	test: {
 		include: ["src/**/*.{test,spec}.{js,ts}"],
 	},
