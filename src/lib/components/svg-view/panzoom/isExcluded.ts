c40ff49f6fffff34c9f1afb62839eac974be3361
Anton <dev@atjn.dk>
2023-11-30 13:18:02 +0100

    Add panzoom disclaimers

--- src/lib/components/svg-view/panzoom/isExcluded.ts
+++ src/lib/components/svg-view/panzoom/isExcluded.ts
@@ -1,3 +1,12 @@
+/**
+ * @file
+ * Code adapted from https://www.npmjs.com/package/@panzoom/panzoom/v/4.5.1
+ * The original code is not very optimized for svelte. CSS values are set at runtime instead of compile time, and events are handled outside of the svelte event loop.
+ * We have forked the code to integrate it correctly with svelte.
+ *
+ * PLEASE DO NOT MODIFY unless it is very necessary. This code is intentionally structured to follow the source code, to make future upgrades/debugging easier.
+ */
+
 import type { PanzoomOptions } from "./types";
 
 function getClass(elem: Element) {
