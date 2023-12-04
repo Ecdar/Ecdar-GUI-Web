45cf5009424f8d97a150c26729050892a4ebafb3
TSKsmiley <arnarfreyr29@gmail.com>
2023-12-04 13:24:55 +0100

    fix

--- src/lib/components/svgView/SvgView.svelte
+++ src/lib/components/svgView/SvgView.svelte
@@ -8,7 +8,8 @@
 	import { activeView } from "$lib/globalState/activeProject";
 	import { Component } from "$lib/classes/automaton/component/Component";
 	import Edge from "./Edge.svelte";
-	import Location from "$lib/components/svg-vie$lib/components/svgView/Location.svelteom "$lib/classes/automaton/system/System";
+	import Location from "$lib/components/svgView/Location.svelte";
+	import { System } from "$lib/classes/automaton";
 
 	/**
 	 * The parent svg element that the entire view is shown with.
