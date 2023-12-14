44c60617a597d5fc094384722c81c41f99e9b60f
Anton <dev@atjn.dk>
2023-12-14 22:11:05 +0100

    whoops

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,6 +12,14 @@ const config: PlaywrightTestConfig = {
 	testMatch: /(.+\.)?(test|spec)\.[jt]s/,
 	projects: [
 		{
+			name: "chromium",
+			use: { ...devices["Desktop Chrome"] },
+		},
+		{
+			name: "firefox",
+			use: { ...devices["Desktop Firefox"] },
+		},
+		{
 			name: "webkit",
 			use: { ...devices["Desktop Safari"] },
 		},
