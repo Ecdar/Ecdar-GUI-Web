2cdc68436644ef9deb6ac21033100ddabee21796
BaBrixx <bastian.b.hansen@gmail.com>
2023-12-14 16:06:09 +0100

    Disabling concurrent tests
    Co-Authored-By: Nyby <45389841+NybyDK@users.noreply.github.com>

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,6 +2,7 @@ import type { PlaywrightTestConfig } from "@playwright/test";
 import { devices } from "@playwright/test";
 
 const config: PlaywrightTestConfig = {
+	workers: 1,
 	webServer: {
 		command: "yarn build && yarn preview",
 		port: 4173,
